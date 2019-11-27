7902b74d0940b26c8d264c0d4fad706bddb764db
Mrs Qu <862792055@qq.com>
2019-11-27 23:06:41 +0800

    收货地址

--- pages/order/index.js
+++ pages/order/index.js
@@ -5,66 +5,115 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+    list: [],
+    count: 1
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function (options) {
-
+  onLoad: function(options) {},
+  add() {
+    let count = this.data.count;
+    count++;
+    this.setData({
+      count
+    });
   },
-	toAddress(){
-		wx.navigateTo({
-			url:'/pages/addressList/index'
-		})
-	},
+  minus() {
+    let count = this.data.count;
+    if (count < 1) {
+      return
+    }
+    count--;
+    this.setData({
+      count
+    });
+  },
+  // 获取当前位置
+  getLocation() {
+    wx.chooseAddress({
+      success: res => {
+        let address = res.postalCode + res.provinceName + res.cityName + res.countyName + res.detailInfo;
+        wx.setStorageSync('userName', res.userName);
+        wx.setStorageSync('telNumber', res.telNumber);
+        wx.setStorageSync('address', address);
+        // 添加地址
+        this.data.list.push({
+          name: res.userName,
+          tel: res.telNumber,
+          address: address,
+          type: 0
+        });
+        this.setData({
+          list: this.data.list
+        })
+        wx.setStorageSync('list', this.data.list);
+      }
+    })
+  },
+  getLocalAddress() {
+    let userName = wx.getStorageSync('userName');
+    let telNumber = wx.getStorageSync('telNumber');
+    let address = wx.getStorageSync('address');
+    this.setData({
+      userName,
+      telNumber,
+      address
+    })
+  },
+  toAddress() {
+    wx.navigateTo({
+      url: '/pages/addressList/index'
+    })
+  },
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
-  onReady: function () {
+  onReady: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
-  onShow: function () {
-
+  onShow: function() {
+    this.getLocalAddress();
   },
+
 
   /**
    * 生命周期函数--监听页面隐藏
    */
-  onHide: function () {
+  onHide: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
-  onUnload: function () {
+  onUnload: function() {
 
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
-  onPullDownRefresh: function () {
+  onPullDownRefresh: function() {
 
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
-  onReachBottom: function () {
+  onReachBottom: function() {
 
   },
 
   /**
    * 用户点击右上角分享
    */
-  onShareAppMessage: function () {
+  onShareAppMessage: function() {
 
   }
 })
