a57fd10c9f1ee64b5e3d3be641aa2e710cfd25fe
Mrs Qu <862792055@qq.com>
2020-02-06 22:26:17 +0800

    物流

--- pages/orderStatus/orderStatus.js
+++ pages/orderStatus/orderStatus.js
@@ -29,6 +29,7 @@ Page({
 	 * 生命周期函数--监听页面加载
 	 */
 	onLoad: function (options) {
+		this.getExpressByNo()
 		if (options.home) {
 			this.setData({
 				toHome: true
@@ -142,6 +143,20 @@ Page({
 	// 		})
 	// 	});
 	// },
+
+	//订单列表
+	getExpressByNo() {
+		request({
+			token: app.globalData.token.prefix + app.globalData.token.token,
+			url: `getExpressByNo`,
+			data: {
+				no: '2020020421121708890000000002'
+			}
+		}).then(res => {
+			if (res.data.code == 200) {
+			}
+		});
+	},
 
 	//订单列表
 	getOrdersList() {
